7ff9a3c63031920cff9920d975feab32be1335fd
vitja2587a1 <55094843+vitja2587a1@users.noreply.github.com>
2020-11-10 12:12:42 +0700

    Update 21dlygussei.py

--- 21dlygussei.py
+++ 21dlygussei.py
@@ -6,7 +6,7 @@ win = pygame.display.set_mode((800, 800))
 
 Suit = ["chern", "byb", "cherv", "krest"]
 Value = ['6', '7', '8', '9', '10', '11', '12', '13', '14']
-f1 = pygame.font.Font('Verdana.ttf', 24)
+f1 = pygame.font.Font('/usr/share/fonts/truetype/msttcorefonts/Arial.ttf', 24)
 
 
 class Card:
@@ -45,7 +45,7 @@ class Player:
         card = deck.get_card()
         win.blit(card.sprite, (x1_card, y1_cards))
         self.cards1.append(card)
-        x1_card += 30
+        x1_card += 25
         pygame.display.update()
 
 
@@ -62,13 +62,13 @@ class Enemy:
         card = deck.get_card()
         win.blit(card.sprite, (x2_card, y2_cards))
         self.cards2.append(card)
-        x2_card += 30
+        x2_card += 25
         pygame.display.update()
 
     def draw_card(self, x2_card, y2_cards):
         for e in self.cards2:
             win.blit(e.sprites, (x2_card, y2_cards))
-            x2_card -= 30
+            x2_card -= 25
             pygame.display.update()
 
 
@@ -89,7 +89,7 @@ class Game:
         if pygame.K_KP_PLUS:
             self.player.new_card(self, koloda, x, y)
             self.enemy.new_card(self, koloda)
-            x += 30
+            x += 25
             while run:
                 print('Хотите взять карту?+/-')
                 text = fi.render('Хотите взять карту?+/-', 1, (100, 100, 100))
@@ -99,7 +99,7 @@ class Game:
                 if pygame.K_KP_PLUS:
                     self.player.new_card(self, koloda, x, y)
                     self.enemy.new_card(self, koloda)
-                    x += 30
+                    x += 25
                 else:
                     n = True
                     while n:
